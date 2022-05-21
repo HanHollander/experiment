20d060becbd687607c136dbac31ae45b0e521ffc
Han Hollander <han_hollander@hotmail.nl>
2022-05-21 21:15:04 +0200

    first setup of CMake project

---
+++ build/CMakeFiles/experiment.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/experiment.dir/main.cpp.o"
+  "experiment"
+  "experiment.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/experiment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
